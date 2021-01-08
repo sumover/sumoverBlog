25cc968d65d21ef8e70244a399134b1425365032
sumover <sumover@foxmail.com>
2021-01-08 20:29:05 +0800

    更新了一些view

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: '猫窝首页'});
 });
 
+router.get('/about', async (req, res, next) => {
+    res.render('about', {title: "猫窝说明"});
+});
 module.exports = router;
