69f68d4e605067adfd0128cf963a4762e46916b4
sumover <sumover@foxmail.com>
2021-01-09 13:22:14 +0800

    添加了登录操作

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,44 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require("body-parser");
 const urlEncodeParser = bodyParser.urlencoded({extend: false});
-
+const userService = require("../services/UserService");
 
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-router.post('/login', urlEncodeParser, async (req, res, next) => {
+router.get('/loginStatus', urlEncodeParser, (req, res, next) => {
+    if (req.session.loginUser) {
+        res.json({message: "user login", username: req.session.loginUser.username});
+    } else res.json({message: "user not login"});
+});
 
+router.post('/login', urlEncodeParser, async (req, res, next) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    let existRes = await userService.userExist(username);
+    if (!existRes) {
+        res.json({
+            message: "user not exist"
+        }).end();
+    } else {
+        let checkRes = await userService.userCheck(username, password);
+        if (checkRes === "password error") {
+            res.json({
+                message: "password error"
+            }).end();
+        } else {
+            req.session.loginUser = checkRes;
+            res.json({
+                message: "login success"
+            }).end();
+        }
+    }
+});
+
+router.get('/logout', async (req, res, next) => {
+    req.session.destroy();
+    res.redirect('/');
 });
 
 router.post('/register', urlEncodeParser, async (req, res, next) => {
--- services/UserService.js
+++ services/UserService.js
@@ -4,11 +4,23 @@ const Sequelize = db.sequelize;
 const UserModel = require("../models/user")(Sequelize, DataTypes);
 
 module.exports = {
-    userCheck: async () => {
-
+    userCheck: async (username, password) => {
+        let queryRes = await UserModel.findAll({
+            where: {
+                username: username,
+                password: password
+            }
+        });
+        if (queryRes.length === 0) return "password error";
+        else return queryRes[0];
     },
-    userExist: async () => {
-
+    userExist: async (username) => {
+        let queryRes = await UserModel.findAll({
+            where: {
+                username: username
+            }
+        });
+        return (queryRes.length !== 0);
     },
     userAdd: async () => {
 
