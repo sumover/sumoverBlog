748e87d7fb6759bf5fad2c37691d1b85787e6a98
sumover <sumover@foxmail.com>
2021-01-09 15:59:30 +0800

    更新了一些首页的option

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: '猫窝大门'});
+    res.render('index', {
+        title: '猫窝大门',
+        isIndex: true
+    });
 });
 
 router.get('/about', async (req, res, next) => {
-    res.render('about', {title: "猫窝说明"});
+    res.render('about', {
+        title: "猫窝说明",
+        isAbout: true
+    });
 });
 module.exports = router;
