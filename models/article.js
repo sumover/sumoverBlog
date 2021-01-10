b57fc209724a99f53fa7955c742e0059aff9507d
sumover <sumover@foxmail.com>
2021-01-11 01:03:00 +0800

    添加了article的model

---
+++ models/article.js
@@ -0,0 +1,51 @@
+const {
+  DataTypes
+} = require('sequelize');
+
+module.exports = sequelize => {
+  const attributes = {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: null,
+      primaryKey: true,
+      autoIncrement: true,
+      comment: null,
+      field: "id"
+    },
+    title: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "title"
+    },
+    createTime: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "createTime"
+    },
+    articleDetail: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      defaultValue: null,
+      primaryKey: false,
+      autoIncrement: false,
+      comment: null,
+      field: "articleDetail"
+    }
+  };
+  const options = {
+    tableName: "article",
+    comment: "",
+    indexes: []
+  };
+  const ArticleModel = sequelize.define("article_model", attributes, options);
+  return ArticleModel;
+};
