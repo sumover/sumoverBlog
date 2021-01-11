67a8e7b38616a2dbdd74162e5db426f08e13cdaa
sumover <sumover@foxmail.com>
2021-01-11 23:13:11 +0800

    修改了当扫描失败时, 报错导致无法运行的bug,  同时使用try..catch..来接受bug

--- router-autodetect.js
+++ router-autodetect.js
@@ -4,7 +4,11 @@ const path = require("path");
 module.exports = (app) => {
     for (let map of AppConfig.urlMapping) {
         console.log(`process router: ${map.url}=>${map.router}.js...`);
-        const router = require(path.join(AppConfig.routerPath, map.router));
-        app.use(map.url, router);
+        try {
+            const router = require(path.join(AppConfig.routerPath, map.router));
+            app.use(map.url, router);
+        } catch (err) {
+            console.log(`router mapping error${map.url}=>${map.router}`);
+        }
     }
 }
