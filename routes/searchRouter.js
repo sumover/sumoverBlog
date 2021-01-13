95c965d44b0def9d00542950d157192d2f223b00
sumover <sumover@foxmail.com>
2021-01-13 19:10:41 +0800

    添加了search功能模块

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -3,12 +3,28 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 const urlEncodeParser = bodyParser.urlencoded({extend: false});
 
-router.get('/', urlEncodeParser, async (req, res, next) => {
-    var renderOption = {
-        title: 'searchResult',
-    }
 
-    res.render('searchResult', renderOption);
-});
+//  service import
+const articleService = require("../services/ArticleService");
+
+
+router.get('/', urlEncodeParser,
+    async (req, res, next) => {
+        var tokenWord = req.query.q;
+        if (tokenWord === "") {
+            var renderOption = {
+                title: 'searchResult',
+                articleSearchResultList: []
+            };
+            res.render('searchResult', renderOption);
+            return
+        }
+        var searchedArticle = await articleService.queryArticleByToken(tokenWord);
+        var renderOption = {
+            title: 'searchResult',
+            articleSearchResultList: searchedArticle
+        };
+        res.render('searchResult', renderOption);
+    });
 
 module.exports = router;
